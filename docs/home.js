476d59abf532fcc65d1133fb0b7cc765ff2558f1
Vulcan41 <161815679+Vulcan41@users.noreply.github.com>
2026-02-26 11:30:16 +0200

    Add files via upload

--- docs/home.js
+++ docs/home.js
@@ -16,3 +16,43 @@ window.logout = async function () {
     await supabase.auth.signOut();
     window.location.href = "index.html";
 };
+
+
+
+
+
+const saveBtn = document.getElementById("save-profile");
+
+saveBtn?.addEventListener("click", async () => {
+
+    // get logged in user
+    const { data: userData } = await supabase.auth.getUser();
+    const user = userData.user;
+
+    if (!user) {
+        alert("No user logged in");
+        return;
+    }
+
+    // read form values
+    const full_name = document.getElementById("test-name").value;
+    const username = document.getElementById("test-username").value;
+    const bio = document.getElementById("test-bio").value;
+
+    // update profile
+    const { error } = await supabase
+        .from("profiles")
+        .update({
+        full_name,
+        username,
+        bio
+    })
+        .eq("id", user.id);
+
+    if (error) {
+        console.error(error);
+        alert("Error saving profile");
+    } else {
+        alert("Profile saved!");
+    }
+});
