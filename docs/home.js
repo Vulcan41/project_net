daa50682c40fbf2dbfb4753e1e963c71f6ed197d
Vulcan41 <161815679+Vulcan41@users.noreply.github.com>
2026-02-26 16:10:28 +0200

    Add files via upload

--- docs/home.js
+++ docs/home.js
@@ -1,9 +1,4 @@
-// home.js
-
 import { supabase } from "./supabase.js";
-import { loadProfile, setupSaveProfile } from "./user.js";
-
-const welcome = document.getElementById("welcome");
 
 /* ---------- SESSION CHECK ---------- */
 
@@ -11,21 +6,29 @@ const {
     data: { session }
 } = await supabase.auth.getSession();
 
-// Protect page
 if (!session) {
     window.location.href = "index.html";
-} else {
-    welcome.innerText = "Welcome " + session.user.email;
 }
+
+const { data: userData } = await supabase.auth.getUser();
+const user = userData.user;
+
+const { data: profile } = await supabase
+    .from("profiles")
+    .select("credits")
+    .eq("id", user.id)
+    .single();
+
+document.getElementById("credits-indicator").textContent =
+"Credits: " + (profile?.credits ?? 0);
 
 /* ---------- LOGOUT ---------- */
 
-window.logout = async function () {
+document.getElementById("logout-btn")
+    .addEventListener("click", async () => {
     await supabase.auth.signOut();
     window.location.href = "index.html";
-};
+});();
 
-/* ---------- PAGE STARTUP ---------- */
+const main = document.getElementById("main-content");
 
-loadProfile();
-setupSaveProfile();
